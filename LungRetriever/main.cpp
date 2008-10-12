c306d4166582dcde68780cd61c73bb6f51b0d446
filipegiusti <filipegiusti@f2da06a6-6d3b-0410-a9c8-4181b64b1b3e>
2008-10-12 19:38:05 +0000

    Background Removal, needs Refactoring.

--- LungRetriever/main.cpp
+++ LungRetriever/main.cpp
@@ -2,6 +2,8 @@
 #include "itkImageFileReader.h"
 #include "itkImageFileWriter.h"
 #include "itkBinaryThresholdImageFilter.h"
+#include "itkNeighborhoodConnectedImageFilter.h"
+#include "itkMaskNegatedImageFilter.h"
 #include "Segmentation.hpp"
 
 #define IMAGE_DIMENSIONS 2
@@ -12,6 +14,8 @@ typedef itk::Image< IMAGE_DATA, IMAGE_DIMENSIONS > ImageType;
 typedef itk::ImageFileReader< ImageType > ReaderType;
 typedef itk::ImageFileWriter< ImageType > WriterType;
 typedef itk::BinaryThresholdImageFilter < ImageType, ImageType >  ThresholdFilterType;
+typedef itk::NeighborhoodConnectedImageFilter< ImageType, ImageType > ConnectedFilterType;
+typedef itk::MaskNegatedImageFilter<ImageType, ImageType, ImageType> MaskNegatedFilterType;
 
 using namespace std;
 
@@ -19,25 +23,26 @@ int main () {
 	ReaderType::Pointer reader = ReaderType::New();
 	reader->SetFileName("IM000011");
 	
-	int thresholdValue = INITIAL_THRESHOLD_VALUE;
-	int thresholdValueOLD = INITIAL_THRESHOLD_VALUE + 1;
+	IMAGE_DATA thresholdValue = INITIAL_THRESHOLD_VALUE;
+	IMAGE_DATA thresholdValueOLD = INITIAL_THRESHOLD_VALUE + 1;
+	ImageType::IndexType index;
 	ThresholdFilterType::Pointer thresholdFilter = ThresholdFilterType::New();
-	thresholdFilter->SetOutsideValue(0);
-	thresholdFilter->SetInsideValue(512); //TODO: magic number
+	thresholdFilter->SetOutsideValue(1); //TODO: magic number
+	thresholdFilter->SetInsideValue(0); 
 	thresholdFilter->SetLowerThreshold(0);
 	thresholdFilter->SetInput(reader->GetOutput());
 	
 	while(thresholdValue != thresholdValueOLD) {
-		int averageAbove, averageBelow, quantityAbove, quantityBelow;
-		ImageType::IndexType index;
+		unsigned long long averageAbove, averageBelow;
+		unsigned int quantityAbove, quantityBelow;
 		
-		quantityAbove = quantityBelow = 0;
+		averageAbove = averageBelow = quantityAbove = quantityBelow = 0;
 		
 		thresholdFilter->SetUpperThreshold(thresholdValue);
 		thresholdFilter->Update();
 
-		int x = thresholdFilter->GetOutput()->GetLargestPossibleRegion().GetSize()[0];
-		int y = thresholdFilter->GetOutput()->GetLargestPossibleRegion().GetSize()[1];
+		unsigned int x = thresholdFilter->GetOutput()->GetLargestPossibleRegion().GetSize()[0];
+		unsigned int y = thresholdFilter->GetOutput()->GetLargestPossibleRegion().GetSize()[1];
 		for (int i = 0; i < x; i++) {
 			for (int j = 0; j < y; j++) {
 				index[0] = i;
@@ -56,13 +61,54 @@ int main () {
 		averageBelow /= quantityBelow;
 		thresholdValueOLD = thresholdValue;
 		thresholdValue = (averageAbove + averageBelow)/2;
-		cout << "thresholdValue: " << thresholdValue << "\n";
-		cout << "thresholdValueOLD: " << thresholdValueOLD << "\n\n";
 	}
+	
+	ConnectedFilterType::Pointer neighborhoodConnected = ConnectedFilterType::New();
+ 	neighborhoodConnected->SetInput( thresholdFilter->GetOutput() );
+	neighborhoodConnected->SetLower(1);
+	neighborhoodConnected->SetUpper(1);
+	neighborhoodConnected->SetReplaceValue(1);//TODO: magic number
+	//Seeds
+	index[0] = 0;
+	index[1] = 0;
+	neighborhoodConnected->SetSeed(index);
+	{
+		unsigned int x = thresholdFilter->GetOutput()->GetLargestPossibleRegion().GetSize()[0];
+		unsigned int y = thresholdFilter->GetOutput()->GetLargestPossibleRegion().GetSize()[1];
+		int i;
+		for (i = 1; i < x; i++) {
+			index[0] = i;
+			index[1] = 0;
+			neighborhoodConnected->AddSeed(index);
+		}
+		for (i = 0; i < x; i++) {
+			index[0] = i;
+			index[1] = y-1;
+			neighborhoodConnected->AddSeed(index);
+		}
+		for (i = 1; i < y-1; i++) {
+			index[0] = 0;
+			index[1] = i;
+			neighborhoodConnected->AddSeed(index);
+		}
+		for (i = 1; i < y-1; i++) {
+			index[0] = x-1;
+			index[1] = i;
+			neighborhoodConnected->AddSeed(index);
+		}
+	}
+	ImageType::SizeType radius;
+ 	radius[0] = 0;
+ 	radius[1] = 0;
+ 	neighborhoodConnected->SetRadius(radius);
+	
+	MaskNegatedFilterType::Pointer maskNegatedFilter = MaskNegatedFilterType::New();
+	maskNegatedFilter->SetInput1(thresholdFilter->GetOutput());
+	maskNegatedFilter->SetInput2(neighborhoodConnected->GetOutput());
 	
 	WriterType::Pointer writer = WriterType::New();
 	writer->SetFileName( "IM000011.dcm" );
-	writer->SetInput(thresholdFilter->GetOutput());
+	writer->SetInput(maskNegatedFilter->GetOutput());
 	
 	writer->Update();
 	
